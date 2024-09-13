933f2739605de090b36942e6dd237b265364c731
Patrick Chaves <patrickchfe@gmail.com>
2024-09-13 16:17:10 -0300

    s

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    return response.json({ message: 'Server is up!' })
+    return res.json({ message: 'Server is up!' })
 })
 
 app.listen(3333)
